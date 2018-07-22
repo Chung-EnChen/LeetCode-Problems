75d17f4a4f3f622006161b745a41aa5d702f0c6b
John Chen <38401589+Chung-EnChen@users.noreply.github.com>
2018-07-22 19:58:02 +0800

    Update "Palindrome Number.java"
    多加了一個判斷statement，負的數字不用檢查直接回傳false

--- Easy_Problem/Palindrome Number.java
+++ Easy_Problem/Palindrome Number.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String temp = String.valueOf(x);                                  //將數字轉成字串
-        for (int i = 0; i < temp.length() / 2; i++){                      //利用迴圈跑整個字串的一半
-            if (temp.charAt(i) != temp.charAt(temp.length() - (i + 1))){  //如果有不相同的字就回傳false
-                return false;
+        if(x < 0)                                                             //負數一定不為palindrome number
+            return false;
+        else{
+            String temp = String.valueOf(x);                                  //將數字轉成字串
+            for (int i = 0; i < temp.length() / 2; i++){                      //利用迴圈跑整個字串的一半
+                if (temp.charAt(i) != temp.charAt(temp.length() - (i + 1))){  //如果有不相同的字就回傳false
+                    return false;
+                }
             }
+            return true;                                                      //跑完沒有遇到不合的就回傳true
         }
-        return true;                                                      //跑完沒有遇到不合的就回傳true
     }
 }
